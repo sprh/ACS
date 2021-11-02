0b009e1c60f5d2cf7e1176608a3fdd3641cebb1a
sktimokhina <sktimokhina@yandex-team.ru>
2021-11-02 14:01:01 +0300

    Added shapes creating from a line; updated the first test

--- Task3/container/containe.py
+++ Task3/container/containe.py
@@ -1,6 +1,6 @@
 from typing import TextIO
 
-from shapes.factory.shape_factory import get_random_shape
+from shapes.factory.shape_factory import get_random_shape, get_shape
 from shapes.shape import Shape
 
 
@@ -15,10 +15,21 @@ class Container:
     @classmethod
     def in_from_file(cls, file: TextIO):
         lines = file.readlines()
+        shapes = []
+        len = 0
+        shape_number = 0
         for line in lines:
-            for item in line.split():
-                print(item)
-        return cls([], 0)
+            shape_number += 1
+            try:
+                shapes.append(get_shape(list(map(int, line.split()))))
+                len += 1
+            except ValueError:
+                print('[Shape {number}] Shape params should be integer values; key should be between '
+                      '1 and 3!'.format(number=shape_number))
+            except IndexError:
+                print('[Shape {number}] Each line should contains all data for '
+                      'a shape!'.format(number=shape_number))
+        return cls(shapes, len)
 
     @classmethod
     def in_random(cls, count: int):
--- Task3/shapes/factory/shape_factory.py
+++ Task3/shapes/factory/shape_factory.py
@@ -1,8 +1,11 @@
+import shapes.color
 from extensions.random_extension import get_random_int
 from shapes.circle import Circle
+from shapes.point import Point
 from shapes.rectangle import Rectangle
 from shapes.shape import Shape
 from shapes.triangle import Triangle
+from shapes.color import get_color_from_int
 
 
 def get_random_shape() -> Shape:
@@ -12,3 +15,16 @@ def get_random_shape() -> Shape:
     elif shape_number == 2:
         return Triangle.get_random_triangle()
     return Circle.get_random_circle()
+
+
+def get_shape(data: [int]) -> Shape:
+    key = data[0]
+    if key == 1:
+        return Rectangle(Point(data[1], data[2]), Point(data[3], data[4]), get_color_from_int(data[5]))
+    elif key == 2:
+        return Triangle(Point(data[1], data[2]), Point(data[3], data[4]), Point(data[5], data[6]),
+                        get_color_from_int(data[7]))
+    elif key == 3:
+        return Circle(data[1], Point(data[2], data[3]), get_color_from_int(data[4]))
+    else:
+        raise ValueError
